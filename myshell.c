d6d36f3913bd9fd3625938585fdfdbfa6d044451
Neha Manoj <143329337+nehamanoj1105@users.noreply.github.com>
2025-09-18 20:07:15 +0530

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -1,15 +1,3 @@
-/* myshell.c
- *
- * Simple Unix-like shell:
- *  - builtins: cd, exit
- *  - pipelines, redirection: > >> <, |
- *  - background jobs with &
- *  - basic signal handling (SIGINT, SIGCHLD)
- *
- * Compile: gcc -Wall -Wextra -std=gnu11 -o myshell myshell.c
- * Run: ./myshell
- */
-
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
